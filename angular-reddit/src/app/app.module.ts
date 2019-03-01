16605bb6febbd6b679b73cdde7c123c188bfcf7b
EC2 Default User <ec2-user@ip-172-31-24-169.ec2.internal>
2019-03-01 08:37:41 +0000

    consegna reddit like app

---
+++ angular-reddit/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; //Aggiungiamo qui 
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent
+  ],
+  imports: [
+    BrowserModule, NgbModule //Aggiungiamolo qui
+  ],
+
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
